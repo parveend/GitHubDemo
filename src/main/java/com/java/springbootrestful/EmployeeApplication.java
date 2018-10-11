110d6215ba8c7ce3fa308f0082e00a4f70539936
parveen kaur <parveen.kaur@zorang.com>
2018-10-11 13:14:33 +0530

    commit message hai

--- src/main/java/com/java/springbootrestful/EmployeeApplication.java
+++ src/main/java/com/java/springbootrestful/EmployeeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EmployeeApplication {
+public class EmployeeApplication {       
 
 	public static void main(String as[])
 	{
